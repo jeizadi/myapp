2ed22be41f29735123bff42b0d7aeb0e154484e9
Jenna Eizadi <jeizadi@gmail.com>
2016-06-01 23:36:29 -0700

    Added Shapes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
 	def index
+		@shapes = [ {type: 'circle', svg_width: '100', svg_height: '100', cx: '50', cy: '50', r: '40', style: "fill:yellow;stroke-width:4;stroke:green"},
+					{type: 'rectangle', svg_width: '400', svg_height: '100', width: '400', height: '100', style: "fill:blue;stroke-width:10;stroke:black"}, 
+					{type: 'star', svg_width: '300', svg_height: '200', points: "100,10 40,198 190,78 10,78 160,198", style: "fill:lime;stroke:purple;stroke-width:5;fill-rule:evenodd;"}
+					]
 		render 'index'
 	end
 end
